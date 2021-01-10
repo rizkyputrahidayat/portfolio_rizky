4640d4563c41d50cee43125f614bc8d5b206b169
rizkyputrahidayat <rizkyptr.h@gmail.com>
2021-01-10 21:12:29 +0700

    scroll commit

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,10 @@
-<?php 
+<?php
 $koneksi = mysqli_connect("localhost", "root", "", "simpan");
 
 // Cek Koneksi
-if (mysqli_connect_errno()){
-    echo "Pesan Tidak Berhasil di Kirim : " . mysqli_connect_errno();  
+if (mysqli_connect_errno()) {
+    echo "Pesan Tidak Berhasil di Kirim : " . mysqli_connect_errno();
 }
+
+// <!--===== SCROLL REVEAL =====-->
+// <!-- <script src="https://unpkg.com/scrollreveal"></script> -->
